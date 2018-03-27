6c0e6348fc6e645113c02e942d6e05c2b77c5238
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2018-03-27 13:54:43 -0400

    chore: Add pre-commit.sh output when run successfully

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "pre-commit hook starting"
 if go test ./...; then
+    echo "pre commit-hook finished."
+    echo "go test return 0"
     exit 0
 else
     echo "Aborting commit go test failed"
