789487d0d590d34dfcbe5f5f21a4b9423a169d8d
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2018-03-28 21:53:16 -0400

    feat: Add gqlschema pkg

--- substancegen/generators/graphqlgo/codeGeneration.go
+++ substancegen/generators/graphqlgo/codeGeneration.go
@@ -109,24 +109,3 @@ func GenGraphqlGoSampleQuery(gqlObjectTypes map[string]substancegen.GenObjectTyp
 	buff.WriteString(bufferString)
 	return buff
 }
-
-/*OutputGraphqlSchema Returns a buffer containing a GraphQL schema in the standard GraphQL schema syntax*/
-func OutputGraphqlSchema(gqlObjectTypes map[string]substancegen.GenObjectType) bytes.Buffer {
-	var buff bytes.Buffer
-
-	graphqlSchemaTemplate := "{{range $key, $value := . }}type {{.Name}} {\n {{range .Properties}}\t{{.ScalarName}}: {{if .IsList}}[{{.ScalarType}}]{{else}}{{.ScalarType}}{{end}}{{if .Nullable}}{{else}}!{{end}}\n{{end}}}\n{{end}}"
-	tmpl := template.New("graphqlSchema")
-	tmpl, err := tmpl.Parse(graphqlSchemaTemplate)
-	if err != nil {
-		log.Fatal("Parse: ", err)
-		return buff
-	}
-	//print schema
-	err1 := tmpl.Execute(&buff, gqlObjectTypes)
-	if err1 != nil {
-		log.Fatal("Execute: ", err1)
-		return buff
-	}
-
-	return buff
-}
