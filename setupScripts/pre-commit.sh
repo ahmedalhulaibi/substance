c0c170b700b3faaf2021c7794cd2d23af5e5e219
ahmed <ahmed.alhulaibi41@gmail.com>
2018-04-15 00:41:56 -0400

    chore: Docker inspect host ports to build environment var

--- setupScripts/pre-commit.sh
+++ setupScripts/pre-commit.sh
@@ -11,9 +11,11 @@ if [ -z "$mysqlcontainerIP" ] || [ -z "$pgsqlcontainerIP" ] ; then
     docker exec -i mysqldbsubstance bash < $GOPATH/src/github.com/ahmedalhulaibi/substance/setupScripts/setenv_mysql.sh
     docker exec -i pgsqldbsubstance bash < $GOPATH/src/github.com/ahmedalhulaibi/substance/setupScripts/setenv_pgsql.sh
 fi
-export SUBSTANCE_MYSQL="root@tcp($mysqlcontainerIP:3306)/delivery"
+msqlcontainerPort=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort}}' mysqldbsubstance)
+export SUBSTANCE_MYSQL="root@tcp($mysqlcontainerIP:$msqlcontainerPort)/delivery"
 echo $SUBSTANCE_MYSQL
-export SUBSTANCE_PGSQL="postgres://travis_test:password@$pgsqlcontainerIP:5432/postgres?sslmode=disable"
+pgqlcontainerPort=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5432/tcp") 0).HostPort}}' pgsqldbsubstance)
+export SUBSTANCE_PGSQL="postgres://travis_test:password@$pgsqlcontainerIP:$pgqlcontainerPort/postgres?sslmode=disable"
 echo $SUBSTANCE_PGSQL
 if go test ./...; then
     echo "pre commit-hook finished."
