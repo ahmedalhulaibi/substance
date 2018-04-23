ee537284ddf5927b202b52ffe261aff441455509
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2018-04-23 01:08:57 -0400

    feat: Generate graphql Create obj mutation

--- substancegen/generators/graphqlgo/codeGeneration_test.go
+++ substancegen/generators/graphqlgo/codeGeneration_test.go
@@ -233,7 +233,8 @@ func main() {
 	fmt.Println("Test with Get	:	curl -g 'http://localhost:8080/graphql?query={ GetCustomer{FirstName,}, }'")
 
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: QueryFields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	rootMutation := graphql.ObjectConfig{Name: "RootMutation", Fields: MutationFields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery), Mutation: graphql.NewObject(rootMutation)}
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
@@ -349,5 +350,100 @@ func TestGenGraphqlGoFieldsGetFunc(t *testing.T) {
 
 	if buff.String() != expectedBuff.String() {
 		t.Errorf("Expected\n\n'%s'\n\nReceived\n\n'%s'\n\n", expectedBuff.String(), buff.String())
+	}
+}
+
+func TestGenGraphqlGoFieldsCreateFunc(t *testing.T) {
+	newGenObjType := substancegen.GenObjectType{Name: "Customer", LowerName: "customer", SourceTableName: "Customers"}
+	newGenObjType.Properties = make(substancegen.GenObjectProperties)
+	newGenObjType.Properties["FirstName"] = &substancegen.GenObjectProperty{
+		IsList:          false,
+		IsObjectType:    false,
+		KeyType:         []string{"PRIMARY KEY"},
+		ScalarName:      "FirstName",
+		ScalarNameUpper: "FirstName",
+		ScalarType:      "string",
+		AltScalarType:   make(map[string]string),
+		Nullable:        false,
+	}
+	newGenObjType.Properties["FirstName"].Tags = make(substancegen.GenObjectTag)
+	newGenObjType.Properties["FirstName"].Tags["json"] = append(newGenObjType.Properties["FirstName"].Tags["json"], "firstName")
+
+	newGenObjType.Properties["PhoneNumber"] = &substancegen.GenObjectProperty{
+		IsList:          false,
+		IsObjectType:    false,
+		KeyType:         []string{"PRIMARY KEY"},
+		ScalarName:      "PhoneNumber",
+		ScalarNameUpper: "PhoneNumber",
+		ScalarType:      "string",
+		AltScalarType:   make(map[string]string),
+		Nullable:        false,
+	}
+	newGenObjType.Properties["PhoneNumber"].Tags = make(substancegen.GenObjectTag)
+	newGenObjType.Properties["PhoneNumber"].Tags["json"] = append(newGenObjType.Properties["PhoneNumber"].Tags["json"], "phoneNumber")
+
+	newGenObjType.Properties["ShoppingList"] = &substancegen.GenObjectProperty{
+		IsList:          true,
+		IsObjectType:    true,
+		KeyType:         []string{""},
+		ScalarName:      "ShoppingList",
+		ScalarNameUpper: "ShoppingList",
+		ScalarType:      "ShoppingList",
+		AltScalarType:   make(map[string]string),
+		Nullable:        false,
+	}
+	newGenObjType.Properties["ShoppingList"].Tags = make(substancegen.GenObjectTag)
+	newGenObjType.Properties["ShoppingList"].Tags["json"] = append(newGenObjType.Properties["ShoppingList"].Tags["json"], "shoppingList")
+
+	genObjMap := make(map[string]substancegen.GenObjectType)
+	genObjMap["Customers"] = newGenObjType
+	var buff bytes.Buffer
+	gqlPlugin := Gql{}
+	InitGraphqlDataTypes(&gqlPlugin)
+	gqlPlugin.PopulateAltScalarType(genObjMap, false, false)
+	GenGraphqlGoFieldsCreateFunc(genObjMap, &buff)
+
+	var expectedBuff bytes.Buffer
+
+	expectedBuff.WriteString(`
+	MutationFields["CreateCustomer"] = &graphql.Field{
+		Type: customerType,
+		Args: graphql.FieldConfigArgument{
+			"FirstName": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+			},
+			"PhoneNumber": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+			},
+			
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			QueryCustomerObj := Customer{}
+			if val, ok := p.Args["FirstName"]; ok {
+				QueryCustomerObj.FirstName = val.(string)
+			}
+			if val, ok := p.Args["PhoneNumber"]; ok {
+				QueryCustomerObj.PhoneNumber = val.(string)
+			}
+			
+			CreateCustomer(DB,QueryCustomerObj)
+			var ResultCustomerObj Customer
+			GetCustomer(DB,QueryCustomerObj,&ResultCustomerObj)
+
+			return ResultCustomerObj, nil
+		},
+	}
+`)
+
+	if buff.String() != expectedBuff.String() {
+		t.Errorf("Expected\n\n'%s'\n\nReceived\n\n'%s'\n\n", expectedBuff.String(), buff.String())
+		t.Errorf("Expected\n\n'%v'\n\nReceived\n\n'%v'\n\n", expectedBuff, buff)
+		expectedBuffBytes := expectedBuff.Bytes()
+		buffBytes := buff.Bytes()
+		for i := range expectedBuffBytes {
+			if expectedBuffBytes[i] != buffBytes[i] {
+				fmt.Printf("%d Expected Char: %c\tReceived Char: %c\n", i, expectedBuffBytes[i], buffBytes[i])
+			}
+		}
 	}
 }
