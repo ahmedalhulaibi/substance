43d28d04a7fb3080a9c750040f34e8af4c65e25b
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-08-09 11:56:54 -0400

    Chg precommit hook to use guest port
    This allows the container to run without exposing the
    port on the host

--- setupScripts/pre-commit.sh
+++ setupScripts/pre-commit.sh
@@ -11,11 +11,9 @@ if [ -z "$mysqlcontainerIP" ] || [ -z "$pgsqlcontainerIP" ] ; then
     docker exec -i mysqldbsubstance bash < $GOPATH/src/github.com/ahmedalhulaibi/substance/setupScripts/setenv_mysql.sh
     docker exec -i pgsqldbsubstance bash < $GOPATH/src/github.com/ahmedalhulaibi/substance/setupScripts/setenv_pgsql.sh
 fi
-msqlcontainerPort=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort}}' mysqldbsubstance)
-export SUBSTANCE_MYSQL="root@tcp($mysqlcontainerIP:$msqlcontainerPort)/delivery"
+export SUBSTANCE_MYSQL="root@tcp($mysqlcontainerIP:3306)/delivery"
 echo $SUBSTANCE_MYSQL
-pgqlcontainerPort=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5432/tcp") 0).HostPort}}' pgsqldbsubstance)
-export SUBSTANCE_PGSQL="postgres://travis_test:password@$pgsqlcontainerIP:$pgqlcontainerPort/postgres?sslmode=disable"
+export SUBSTANCE_PGSQL="postgres://travis_test:password@$pgsqlcontainerIP:5432/postgres?sslmode=disable"
 echo $SUBSTANCE_PGSQL
 if go test ./...; then
     echo "pre commit-hook finished."
